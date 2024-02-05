666ea6eb8a66c2022daefde7ae4fee6dc521a757
Tomi <hernandeztomas584@gmail.com>
2024-02-05 09:01:51 -0300

    feat: compression

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-
+  /* Using compression helps to decrease the size of the response body. DON'T use it if you have a reverse-proxy as NGINX, or Apache. */
+  app.use(compression());
   app.use(helmet());
 
   await app.listen(process.env.PORT);
